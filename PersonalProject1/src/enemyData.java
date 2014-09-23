f3a482bad5142e6d27724e88df48289c756a7dd8
Connor <joyceco1@mullenmail.com>
2014-09-23 17:37:26 -0600

    fixed the heal dungeon part, error with array

--- PersonalProject1/src/enemyData.java
+++ PersonalProject1/src/enemyData.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
-//use a counter and an array to randomize the monsters, finish the heal part of the method
 
 public class enemyData
 	{
 	public static int levelCount = 0;
-
+    String[] dungeonMonsters;
+    dungeonMonsters = new String[4];
+    dungeonMonsters[0] = "Skeleton Warrior";
+    dungeonMonsters[1] = "Night Demon";
+    dungeonMonsters[2] = "Ogre";
+    dungeonMonsters[3] = "Great Demon";
+    		
 	
 	public static void dungeon()
 	{
@@ -22,7 +27,95 @@ public class enemyData
 					{
 					textAdventure.health = 25;
 					System.out.println("You have been healed to your full strength.");
+					System.out.println("The final monster approaches you.");
+					System.out.println("Prepare to fight, Gursev Master of the Dugneon.");
+		            Scanner keyboard = new Scanner(System.in);		
+					System.out.println("The monster attacks! Will you fight with attacks, magic, or try to counter it?");
+					boolean fightContinues = true;
+					do
+					{
+					String fightDecision = keyboard.next();
+					if(fightDecision.equals ("magic"))
+					{
+						textAdventure.bossHealth = textAdventure.bossHealth - GameData.data.get(textAdventure.statCounter).getAbility();
+						if(textAdventure.bossHealth <= 0)
+						{
+							System.out.println("You have summoned all of your magical ability and struck a killing blow on the Master of the Dungeon.");
+							System.out.println("Congratulations, you have rid the world of this evil and embraced your destiny.");
+							System.out.println("You have beaten the dungeon ending.");
+							System.out.println("THE END");
+							System.exit(0);
+							fightContinues = false;
+							textAdventure.bossHealth = -1;
+		
+							
+						}
+						if(textAdventure.bossHealth >= 0)
+						{
+							System.out.println("The monster now has " + textAdventure.bossHealth + " health remaining. It now attacks you!");
+							textAdventure.health = textAdventure.health - textAdventure.bossAttack;
+							System.out.println("The monster hits you for " + textAdventure.bossAttack + " damage. You now have " + textAdventure.health + " remaining");
+							if(textAdventure.health <=0)
+							{
+								System.out.println("You have been slain");
+								System.out.println("The Master of the Dungeon has finished you, and sets his sights on the rest of the world");
+								System.out.println("GAME OVER");
+								System.exit(0);
+							}
+						}
 					}
+					if(fightDecision.equals("attacks"))
+						{
+						textAdventure.bossHealth = textAdventure.bossHealth - GameData.data.get(textAdventure.statCounter).getAttack();
+						if(textAdventure.bossHealth <= 0)
+							{
+							System.out.println("You use your superior strength and speed to dart around the Dungeon Master eventually finishing him.");
+							System.out.println("You have finished the Dugneon Master and fulfilled your destiny. Congratulations");
+							System.out.println("You finished the dungeon ending.");
+							System.out.println("THE END");
+							System.exit(0);
+							fightContinues = false;
+							textAdventure.bossHealth = -1;
+							}
+						if(textAdventure.bossHealth >=0)
+							{
+							System.out.println("The monster now has " + textAdventure.bossHealth + " health remaining. It now attacks you!");
+							textAdventure.health = textAdventure.health - textAdventure.bossAttack;
+							System.out.println("The monster hits you for " + textAdventure.bossAttack + " damage. You now have " + textAdventure.health + " remaining.");
+							if(textAdventure.health <= 0)
+								{
+								System.out.println("The monster has struck back, fatally wounding you. You have been slain.");
+								System.out.println("You were unable to fulfill your destiny.");
+								System.out.println("GAME OVER");
+								System.exit(0);
+								}
+							}
+						}
+					if(fightDecision.equals("counter"))
+						{
+						int dodgeChance = (int)(Math.random()*5+1);
+						if(dodgeChance == (3))
+							{
+							System.out.println("You successfully evade the monster's attack and strike back, killing it instantly.");
+							System.out.println("The Dungeon Master was no match for your incredibly quick reflexes. He was taken unawares and fell to your strength.");
+							System.out.println("Congratulations you have beaten the dungeon ending.");
+							System.out.println("THE END");
+							System.exit(0);
+							
+							fightContinues = false;
+							}
+						else{
+						System.out.println("You try to counter the monster's attack but fail to time it correctly, before you can recover the monster strings attacks one after the other on you until you finally die.");
+						System.out.println("You were unable to match the Dungeon Master's reflexes and fell to him in battle");
+						System.out.println("GAME OVER");
+						System.exit(0);
+						}
+					}
+					}
+					
+				while(fightContinues);
+				}
+					
 				if(heal.equals("no"))
 					{
 					System.out.println("You have decided to fight the final monster in your weakened state, he will not be any easier.");
@@ -201,6 +294,8 @@ public class enemyData
 	}
 	
 	}
+	}
 }
+	
 
 
